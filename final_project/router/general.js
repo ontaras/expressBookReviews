56d3c6b8cc953d0e50c86be594cee363d461a0c4
ontaras <ontaras@yahoo.com>
2024-04-11 07:06:06 -0400

    logic added

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,26 +17,64 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  let booksNumber = Object.keys(books).length;
+  if(isbn < 1 || isbn > booksNumber) {
+    return res.send(`ISBN not found, enter a number from 1 to ${books_number}`);
+  }
+  else {
+    res.send(books[isbn]);
+  }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author;
+  let authors = [];
+  booksNumber = Object.keys(books).length;
+  
+  for(let i = 1; i < booksNumber; i++) {
+    if(books[i]['author'] === author) {
+        authors.push(books[i]);
+    }
+  }
+  if(!authors.length) {
+    res.send("Author not found");
+  }
+  else {
+    res.send(authors);
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let title = req.params.title;
+    let titles = [];
+    booksNumber = Object.keys(books).length;
+    
+    for(let i = 1; i < booksNumber; i++) {
+      if(books[i]['title'] === title) {
+          titles.push(books[i]);
+      }
+    }
+    if(!titles.length) {
+      res.send("Title not found");
+    }
+    else {
+      res.send(titles);
+    }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let isbn = req.params.isbn;
+    let booksNumber = Object.keys(books).length;
+    if(isbn < 1 || isbn > booksNumber) {
+      return res.send(`ISBN not found, enter a number from 1 to ${books_number}`);
+    }
+    else {
+      res.send(books[isbn]['reviews']);
+    }
 });
 
 module.exports.general = public_users;
